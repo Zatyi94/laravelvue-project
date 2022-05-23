f658b9e121952cc8bc89bc283c2df36ab4618204
Sipos Sándor <sipos.sandor@blathy.info>
2022-05-23 21:08:54 +0200

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <br>
+    <hr>
+    Hello VueJS!
+    <hr>
+    <CounterButton :startv="12" :min="1" :max="13"></CounterButton>
+    <br>
+    <counter-button :startv="2"></counter-button>
+</template>
+
+<script>
+import CounterButton from "./components/CounterButton";
+
+export default {
+    name: "App.vue",
+    components: {
+        CounterButton
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/CounterButton.vue
@@ -0,0 +1,51 @@
+<template>
+    <button @click="decrement"> - </button>
+    {{ number }}
+    <button @click="increment"> + </button>
+    <button @click="number=startValue"> Clear </button>
+</template>
+
+<script>
+export default {
+    name: "CounterButton",
+    /* props: ['startv', 'min', 'max'], // jelzem a komponensemnek, hogy számítson vmilyen paraméterre */
+    props: {
+        startv: Number,
+        min: {
+            type: Number,
+            default: 0
+        },
+        max: {
+            type: Number,
+            default: 15
+        }
+    },
+    data() {
+        return {
+            number: isFinite(this.startv) ? this.startv : 0
+        };
+    },
+    computed: { // adattagok
+        startValue(){
+            return isFinite(this.startv) ? this.startv : 0;
+        }
+    },
+    methods: {
+      increment(){
+          console.log(this.max);
+          if (this.number < this.max){
+              this.number++;
+          }
+      },
+      decrement(){
+          if (this.number > this.min){
+              this.number--;
+          }
+      }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
